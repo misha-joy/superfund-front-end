a408b1cfc5c53567d0e233945aae7f5e4446e410
Reyna Diaz <reynamdiazz@gmail.com>
2023-01-25 21:15:20 -0500

    "adds custom markers to map component"

--- superfund/src/components/Map.js
+++ superfund/src/components/Map.js
@@ -1,30 +1,30 @@
-import React, { Component, useState } from "react";
-import locations from "../data/location.json";
-import PropTypes from "prop-types";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-import CustomMarker from "../assets/warning-icon.jpg";
+import React, { Component, useState } from 'react';
+import locations from '../data/location.json';
+import PropTypes from 'prop-types';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+// import { MarkerClusterer } from '@googlemaps/markerclusterer';
+import CustomMarker from '../assets/Blue.png';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const markers = locations.map((item) => {
   return (
     <Marker
-      key={item["SITE_NAME"]}
+      key={item['SITE_NAME']}
       position={{
-        lat: item["LATITUDE"],
-        lng: item["LONGITUDE"],
+        lat: item['LATITUDE'],
+        lng: item['LONGITUDE'],
       }}
       options={{
-        icon: { CustomMarker },
+        icon: CustomMarker,
       }}
     />
   );
 });
 
 const mapStyles = {
-  width: "45%",
-  height: "45%",
+  width: '45%',
+  height: '45%',
 };
 export class MapContainer extends Component {
   render() {
